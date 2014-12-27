b4882d0e51405d88af987cc81075067162db4f8c
Ben Peachey <potherca@hotmail.com>
2014-12-27 15:52:59 +0100

    Minor improvement to Express code example.

--- node.js-noob-level/example-scripts/hello-www-world-express.js
+++ node.js-noob-level/example-scripts/hello-www-world-express.js
@@ -1,11 +1,12 @@
-var express = require('express');
-var app = express();
+var express, app, server;
+
+express = require('express');
+app = express();
 
 app.get('/hello-world.txt', function(request, response){
     response.send('Hello World!');
 });
 
-var server = app.listen(1337, function() {
+server = app.listen(1337, function() {
     console.log('Listening on port %d', server.address().port);
 });
-
